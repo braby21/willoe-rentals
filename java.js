2d877ed52a6f35c227279bcb2f69d0d059f843d3
braby21 <braby21@gmail.com>
2015-10-30 21:46:35 -0700

    adds CSS transition and some js to the form so there is no error

---
+++ java.js
@@ -0,0 +1,8 @@
+function clickSubmit() {
+    var name_element = document.getElementById("txt_name");
+    var name = name_element.value + " ";	// added blank space to put space before but if they give a name
+    alert("Sorry " + name + "but this is form not functioning at this time.");	// if they don't give a name, no extra space
+};
+
+
+
